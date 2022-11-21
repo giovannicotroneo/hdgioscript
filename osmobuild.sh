723865bd2d8358b44f1f33dae14b7adc79602040
giovannicotroneo <43828773+giovannicotroneo@users.noreply.github.com>
2022-11-21 23:46:16 +0100

    Update osmobuild.sh

--- osmobuild.sh
+++ osmobuild.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+
+cd ~
+git clone https://github.com/osmocom/libosmocore.git
+cd libosmcore
+autoreconf -fi
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig" 
+./configure
+make
+make check
+sudo make install
+sudo ldconfig
+
