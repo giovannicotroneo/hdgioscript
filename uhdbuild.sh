9965778a14b493ff050442c7a09ce045181faf81
Giovanni Cotroneo <giovanni.cotroneo@comtestwireless.eu>
2022-11-21 15:54:04 +0100

    Add script to build uhd from source

---
+++ uhdbuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ~
+
+sudo apt-get install autoconf automake build-essential ccache cmake cpufrequtils doxygen ethtool \
+g++ git inetutils-tools libboost-all-dev libncurses5 libncurses5-dev libusb-1.0-0 libusb-1.0-0-dev \
+libusb-dev python3-dev python3-mako python3-numpy python3-requests python3-scipy python3-setuptools \
+python3-ruamel.yaml 
+
+git clone https://github.com/EttusResearch/uhd.git
+cd uhd/host
+mkdir build
+cd build
+cmake ../
+make 
+make test
+
+sudo make install
+sudo ldconfig
+
+sudo uhd_images_downloader
+
