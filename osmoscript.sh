f9a2f1082e022780a727436b1c423004ffd79225
giovannicotroneo <43828773+giovannicotroneo@users.noreply.github.com>
2022-11-28 10:22:50 +0100

    Update osmoscript.sh

--- osmoscript.sh
+++ osmoscript.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # Ubuntu 20.04.5 LTS
+
+cd ~
+mkdir osmo
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-bsc.cfg
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-bts.cfg
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-hlr.cfg
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-mgw.cfg
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-msc.cfg
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-stp.cfg
+wget https://raw.githubusercontent.com/giovannicotroneo/hdgioscript/master/osocfg/osmo-trx.cfg
