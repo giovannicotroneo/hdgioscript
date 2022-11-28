7ea4c360f421e8a3e558439167d6ad0851d26279
giovannicotroneo <43828773+giovannicotroneo@users.noreply.github.com>
2022-11-28 12:39:37 +0100

    Update osmo.sh

--- osmo.sh
+++ osmo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Ubuntu 20.04.5 LTS
 
-sudo apt install uhd-host osmo-trx osmo-bts osmo-bsc osmo-msc osmo-mgw osmo-hlr osmo-stp
+sudo apt install -y uhd-host osmo-trx osmo-bts osmo-bsc osmo-msc osmo-mgw osmo-hlr osmo-stp
 
